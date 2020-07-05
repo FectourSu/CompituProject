7e33b25ccb72b10fe61b0a030b92d3a047ea9469
TheWayToJunior <developermicrosoft1932@gmail.com>
2020-07-05 21:12:27 +0300

    Сhange the modal

---
+++ CompituProject/Shared/Modal.razor.cs
@@ -0,0 +1,33 @@
+﻿using CompituProject.Models;
+using CompituProject.Services;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace CompituProject.Shared
+{
+    public partial class Modal
+    {
+        [Inject]
+        private IModalService modal { get; set; }
+
+        [Parameter]
+        public IEnumerable<ToDoListModel> ToDoLists { get; set; }
+
+        private string cssClassShow = string.Empty;
+
+        protected override void OnInitialized()
+        {
+            modal.Update += Update;
+        }
+
+        public async void Update()
+        {
+            if (modal.IsOpen.Value)
+                cssClassShow = string.Empty;
+            else
+                cssClassShow = "show";
+
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+}
