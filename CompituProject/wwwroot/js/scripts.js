5ec5939add179a116144c9de02d52e4700b2d2d0
FectourSu <59044810+FectourSu@users.noreply.github.com>
2020-07-25 13:57:13 +0300

    Notification mobile

--- CompituProject/wwwroot/js/scripts.js
+++ CompituProject/wwwroot/js/scripts.js
@@ -16,19 +16,12 @@ function datePush(todo) {
             for (var j = 0; j < arr.length; j++) {
                 if (arr[i] == new Date().toLocaleDateString() || arr[i] < new Date().toLocaleDateString() && arr[i] != "01.01.1")
                 {
-                    notifyMe();
+                    showNotification();
                     return;
                 }
             }
         }
     }
-}
-
-function notifyMe() {
-    var notification = new Notification("Compitu notifice", {
-        tag: "ache-mail",
-        body: "Complete task, please :)"
-    });
 }
 
 window.blazorNotifSet = (todo) => {
@@ -44,4 +37,19 @@ window.blazorNotifSet = (todo) => {
                 datePush(todo);
         });
     }
+}
+
+function showNotification() {
+    Notification.requestPermission(function (result) {
+        if (result === 'granted') {
+            navigator.serviceWorker.ready.then(function (registration) {
+                registration.showNotification('Compitu notifice', {
+                    body: 'Complete task, please :)',
+                    icon: 'https://psv4.userapi.com/c856336/u155561278/docs/d13/b27e672b1446/document.png?extra=baPj9Vj32e-aWBnSX_ZYoKdF923s3j8yojbncD1yWwE8IO-R5l6NcV0v2zy9GzXa_Qr0LdItjzt05Ju1TxYaZRx199iLZynU0jYWsfXTNgjPIY5HM2pYN5w5pLwBKN42cijm5JiAgIDv7CLUb5odgJBg',
+                    vibrate: [200, 100, 200, 100, 200, 100, 200],
+                    tag: 'vibration-sample'
+                });
+            });
+        }
+    });
 }
