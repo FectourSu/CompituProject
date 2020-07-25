412b0100b456bec09cf49e7fbe6967aca4adee5c
FectourSu <59044810+FectourSu@users.noreply.github.com>
2020-07-25 18:25:47 +0300

    Fixed notification

--- CompituProject/wwwroot/js/scripts.js
+++ CompituProject/wwwroot/js/scripts.js
@@ -5,23 +5,31 @@ window.blazorLocalStorage = (arr) => {
 window.blazorReadLocalStorage = () => {
     return localStorage.getItem("arr") ? JSON.parse(localStorage.getItem("arr")) : [];
 }
-function datePush(todo) {
-    let arr = [];
-    for (var i = 0; i < todo.length; i++) {
-        if (!todo[i].completed)
-        {
-            var time = todo[i].dateTime;
-            arr[i] = new Date(time).toLocaleDateString();
 
-            for (var j = 0; j < arr.length; j++) {
-                if (arr[i] <= new Date().toLocaleDateString() && arr[i] != "01.01.1")
+function datePush(todo) {
+    let dates = [];
+    let notCompleted = todo.filter(s => !s.completed);
+    for (var i = 0; i < notCompleted.length; i++) {
+        var time = notCompleted[i].dateTime;
+        dates[i] = new Date(time);
+
+        for (var j = 0; j < dates.length; j++) {
+            if (dates[i] <= new Date() && dates[i] != "01.01.1")
                 {
-                    showNotification();
+                    console.log('what');
+                    showNotification(notCompleted[i].title);
+                    showPCnotification(notCompleted[i].title);
                     return;
                 }
             }
-        }
     }
+}
+
+function showPCnotification(title) {
+    var notification = new Notification("Compitu notifice", {
+        tag: "ache-mail",
+        body: "Время задачи: " + title + " истекло"
+    });
 }
 
 window.blazorNotifSet = (todo) => {
@@ -32,10 +40,10 @@ window.blazorNotifSet = (todo) => {
     });
 }
 
-function showNotification() {
-    navigator.serviceWorker.ready.then(function (registration) {
+function showNotification(title) {
+    var workerContainerInstance = navigator.serviceWorker.ready.then(function (registration) {
         registration.showNotification('Compitu notifice', {
-            body: 'Complete task, please :)',
+            body: "Время задачи: " + title + " истекло",
             icon: 'https://psv4.userapi.com/c856336/u155561278/docs/d13/b27e672b1446/document.png?extra=baPj9Vj32e-aWBnSX_ZYoKdF923s3j8yojbncD1yWwE8IO-R5l6NcV0v2zy9GzXa_Qr0LdItjzt05Ju1TxYaZRx199iLZynU0jYWsfXTNgjPIY5HM2pYN5w5pLwBKN42cijm5JiAgIDv7CLUb5odgJBg',
             vibrate: [200, 100, 200, 100, 200, 100, 200],
             tag: 'vibration-sample'
